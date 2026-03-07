6550f3a5ca28fd385c415ac64828b53b09939cfe
Karina Sedova <karina.sed@yandex.ru>
2026-03-07 21:02:27 +0300

    feat: update cors allowed origins

--- tandem_backend/settings.py
+++ tandem_backend/settings.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
 
 # cors_headers
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    origin.strip() 
+    for origin in os.getenv('CORS_ALLOWED_ORIGINS', '').split(',') 
+    if origin.strip()
 ]

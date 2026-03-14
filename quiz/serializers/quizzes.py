f355040c87155c2fdc277e3373ee700793cebe23
Karina Sedova <karina.sed@yandex.ru>
2026-03-14 19:01:07 +0300

    feat: add serializers for quizzes

---
+++ quiz/serializers/quizzes.py
@@ -0,0 +1,67 @@
+# quiz/serializers/quizzes.py
+
+from rest_framework import serializers
+from quiz.models.quizzes import Quiz, CodeCompletionQuestion, AsyncSorterQuestion
+
+class CodeCompletionQuestionSerializer(serializers.ModelSerializer):
+    hint = serializers.SerializerMethodField()
+
+    class Meta:
+        model = CodeCompletionQuestion
+        fields = ('id', 'code', 'blanks', 'hint')
+
+    def get_hint(self, obj):
+        if not obj.hint_ru and not obj.hint_en:
+            return None
+        return {
+            'ru': obj.hint_ru or "",
+            'en': obj.hint_en or ""
+        }
+
+
+class AsyncSorterQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AsyncSorterQuestion
+        fields = ('id', 'code', 'blocks')
+
+
+class QuizListSerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='quiz_type.name')
+    difficulty = serializers.IntegerField(source='difficulty.level')
+    section = serializers.CharField(source='section.name')
+    
+    title = serializers.SerializerMethodField()
+    tags = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Quiz
+        fields = ('id', 'type', 'difficulty', 'section', 'time_limit', 'title', 'tags')
+
+    def get_title(self, obj):
+        return {
+            'ru': obj.title_ru,
+            'en': obj.title_en
+        }
+
+    def get_tags(self, obj):
+        return [tag.name for tag in obj.tags.all()]
+
+
+class QuizDetailSerializer(QuizListSerializer):
+    questions = serializers.SerializerMethodField()
+
+    class Meta(QuizListSerializer.Meta):
+        fields = QuizListSerializer.Meta.fields + ('questions',)
+
+    def get_questions(self, obj):
+        quiz_type_name = obj.quiz_type.name
+        
+        if quiz_type_name == 'code completion':
+            questions = obj.code_completion_questions.all()
+            return CodeCompletionQuestionSerializer(questions, many=True).data
+            
+        elif quiz_type_name == 'async sorter':
+            questions = obj.async_sorter_questions.all()
+            return AsyncSorterQuestionSerializer(questions, many=True).data
+            
+        return []

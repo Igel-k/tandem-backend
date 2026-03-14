67fec28665766666f19a824d4dafcaa5bd7ea9a2
Karina Sedova <karina.sed@yandex.ru>
2026-03-14 13:42:20 +0300

    feat: add base models for quizzes

---
+++ quiz/models/core.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Section(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Section title')
+
+    class Meta:
+        verbose_name = 'Section'
+        verbose_name_plural = 'Sections'
+        db_table = 'quiz"."section'
+
+    def __str__(self):
+        return self.name
+
+class Difficulty(models.Model):
+    level = models.PositiveSmallIntegerField(unique=True, verbose_name='Difficulty level number')
+    description = models.CharField(max_length=50, verbose_name='Difficulty level text', blank=True)
+
+    class Meta:
+        verbose_name = 'Difficulty'
+        verbose_name_plural = 'Difficulties'
+        ordering = ['level']
+        db_table = 'quiz"."difficulty'
+
+    def __str__(self):
+        return str(self.level)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Tag title')
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+        db_table = 'quiz"."tag'
+
+    def __str__(self):
+        return self.name

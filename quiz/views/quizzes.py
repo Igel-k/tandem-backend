c85872971c1c8621a37972f75347f319872511d2
Karina Sedova <karina.sed@yandex.ru>
2026-03-15 13:12:37 +0300

    feat: add pagination to quiz list

--- quiz/views/quizzes.py
+++ quiz/views/quizzes.py
@@ -1,11 +1,18 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from quiz.models.quizzes import Quiz
 from quiz.serializers.quizzes import QuizListSerializer, QuizDetailSerializer
 
+class QuizListPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 20
+
 class QuizListView(generics.ListAPIView):
-    queryset = Quiz.objects.all()
+    queryset = Quiz.objects.all().order_by('id')
     serializer_class = QuizListSerializer
     permission_classes = [] 
+    pagination_class QuizListPagination
 
 class QuizDetailView(generics.RetrieveAPIView):
     queryset = Quiz.objects.all()

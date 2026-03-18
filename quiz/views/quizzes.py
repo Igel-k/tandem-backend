2651ebdcbbdf39d65cb7665d4dd7a3bb98a96328
Karina Sedova <karina.sed@yandex.ru>
2026-03-18 21:01:52 +0300

    feat: add filter to quiz list view

--- quiz/views/quizzes.py
+++ quiz/views/quizzes.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 from quiz.models.quizzes import Quiz
 from quiz.serializers.quizzes import QuizListSerializer, QuizDetailSerializer
+from quiz.filters.quizzes import QuizFilter
 
 class QuizListPagination(PageNumberPagination):
     page_size = 5
@@ -13,6 +15,8 @@ class QuizListView(generics.ListAPIView):
     serializer_class = QuizListSerializer
     permission_classes = [] 
     pagination_class = QuizListPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = QuizFilter
 
 class QuizDetailView(generics.RetrieveAPIView):
     queryset = Quiz.objects.all()

8e3cb4df8c419c152fde8c423c90f6aa5f8a159f
Karina Sedova <karina.sed@yandex.ru>
2026-03-23 20:47:44 +0300

    feat: add total pages to response

--- quiz/views/quizzes.py
+++ quiz/views/quizzes.py
@@ -17,6 +17,15 @@ class QuizListPagination(PageNumberPagination):
     page_size_query_param = 'page_size'
     max_page_size = 20
 
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
+
 class QuizListView(generics.ListAPIView):
     queryset = Quiz.objects.all().order_by('id')
     serializer_class = QuizListSerializer

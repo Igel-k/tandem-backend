d138563fb3f1bb6885edf6ff34fdb3837d9cb696
Karina Sedova <karina.sed@yandex.ru>
2026-03-23 20:13:57 +0300

    feat: register model for quiz results

--- quiz/models/__init__.py
+++ quiz/models/__init__.py
@@ -1,2 +1,3 @@
 from .core import Section, Difficulty, Tag, QuizType
 from .quizzes import Quiz, CodeCompletionQuestion, AsyncSorterQuestion, SingleChoiceQuestion, TrueFalseQuestion
+from .interactions import QuizResult

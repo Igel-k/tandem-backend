539e625fac0db2379ae0481f78961ee4213f3b1e
Karina Sedova <karina.sed@yandex.ru>
2026-03-28 12:31:41 +0300

    feat: add favorites quizzes to section filter field

--- quiz/filters/quizzes.py
+++ quiz/filters/quizzes.py
@@ -4,8 +4,19 @@ from quiz.models.quizzes import Quiz
 class QuizFilter(django_filters.FilterSet):
     difficulty = django_filters.NumberFilter(field_name='difficulty__level')
     quiz_type = django_filters.CharFilter(field_name='quiz_type__name')
-    section = django_filters.CharFilter(field_name='section__name')
+    section = django_filters.CharFilter(method='filter_section')
 
     class Meta:
         model = Quiz
         fields = ['difficulty', 'quiz_type', 'section']
+
+    def filter_section(self, queryset, name, value):
+        if value.lower() == 'favorites':
+            user = getattr(self, 'request', None) and self.request.user
+            
+            if user and user.is_authenticated:
+                return queryset.filter(favorited_by__user=user)
+            else:
+                return queryset.none()
+
+        return queryset.filter(section__name=value)

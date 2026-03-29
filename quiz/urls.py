56283455836c6901c713b1a0f0c5ed0ac49e716f
Karina Sedova <karina.sed@yandex.ru>
2026-03-29 09:41:20 +0300

    feat: register path for dashboard

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views.quizzes import QuizListView, QuizDetailView, QuizSubmitView
 from .views.interactions import QuizFavoriteToggleView
+from .views.dashboard import UserDashboardView
 
 urlpatterns = [
     path('', QuizListView.as_view(), name='quiz_list'),
     path('<int:pk>/', QuizDetailView.as_view(), name='quiz_detail'),
     path('<int:pk>/submit/', QuizSubmitView.as_view(), name='quiz_submit'),
     path('<int:pk>/favorite/', QuizFavoriteToggleView.as_view(), name='quiz_favorite_toggle'),
+    path('dashboard/', UserDashboardView.as_view(), name='dashboard'),
 ]
